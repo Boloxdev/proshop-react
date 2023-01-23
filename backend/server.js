7bfead2177f5ade1b9885a06c035e94ee2e98e37
Jaume Bolo <bolox.dev@gmail.com>
2023-01-23 06:33:51 +0100

    error handler added

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import colors from 'colors'
 import mongoose from 'mongoose'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoute.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config();
 
@@ -16,6 +17,9 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running on port ${PORT} on ${process.env.NODE_ENV} mode`));
